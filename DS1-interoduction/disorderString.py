c15a611b2061675ebad967e067ae1334a1af63d6
ning <1012907673@qq.com>
2020-03-02 15:39:19 +0800

    'leetCode中级,乱序字符串检查'

---
+++ DS1-interoduction/disorderString.py
@@ -0,0 +1,83 @@
+"""
+乱序字符串是指一个字符串只是另一字符串的重新排序
+前提：字符串由26个小写字母集合组成，长度相同
+比如：python typhon  head  deah
+目的：
+        写一个布尔函数（返回值是布尔值函数）
+        solutionsl('abcd','dbca')
+"""
+# 第一种  穷举法：排除。原因：如果字符串过长，20个长度  20！
+
+
+# 第二种  检查  第一个字符串是不是出现在第二个字符串中  o(n^2)
+def solutions(s1, s2):
+    alist = list(s2)
+    pos1 = 0
+    flag = True
+    while flag and pos1<len(s1):        # n
+        pos2 = 0
+        found = False
+        while pos2<len(s2) and not found:   # n     n^2
+            if s1[pos1] == alist[pos2]:
+                found = True
+            else:
+                pos2 = pos2+1
+        if found:
+            alist[pos2] = None
+            pos1 = pos1 +1
+        else:
+            flag = False
+    return flag
+
+print(solutions('abcd','dbca'))
+
+
+# 第三种   技术和比较法
+# 计算每个字符出现的次数
+# 复杂度 T（n）=n+n+26 = 2n+26  o(n)
+def solutions2(s1,s2):
+    c1 = [0]*26  # 记录s1中字母出现的次数
+    c2 = [0]*26  # 记录s2中字母出现的次数
+    # ord() 返回的是字符在阿斯克码中的数字
+    for i in range(len(s1)):
+        pos = ord(s1[i]) - ord('a')  # n
+        c1[pos] = c1[pos] + 1
+
+    for i in range(len(s2)):
+        pos = ord(s2[i]) - ord('a')  # n
+        c2[pos] = c2[pos] + 1
+    count = 0  # 如果某个字符在s1和s2中出现的次数相同，那么count+1
+    flag = True
+    while count < 26 and flag:
+        if c1[count] == c2[count]:
+            count = count+1   # 26
+        else:
+            flag = False
+    return flag
+
+print(solutions2('aaaabbbbcccc','ccccbbbbaaaa'))
+
+
+# 第四种  排序比较  ： s1,s2 他们都是由完全相同的字符串组成的。
+# 我们可以按照从a-z 排列每一个字符串，如果两个字符串相同，那这两个字符串就是乱序字符串
+# 复杂度 ： O(n^2)
+def solutions3(s1, s2):
+    alist1 = list(s1)
+    alist2 = list(s2)
+    # 排序  复杂度O（n^2）或者O(logn)
+    alist1.sort()
+    alist2.sort()
+
+    flag = True
+    pos = 0
+    while pos < len(s1) and flag:
+        if alist1[pos] == alist2[pos]:
+            pos = pos + 1           # n
+        else:
+            flag = False
+    return flag
+
+print(solutions3('aaaabbbbcccc', 'bbbbaaaacccc'))
+
+
+# 第五种
