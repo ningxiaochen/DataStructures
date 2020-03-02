d9363d0dee66119eeb3dbe68f7fa0e904ade692c
ning <1012907673@qq.com>
2020-03-02 10:30:15 +0800

    '算法分析,大O符号'

---
+++ DS1-interoduction/practise02.py
@@ -0,0 +1,21 @@
+"""
+T(n)=3*n^2+2n+4
+支取变化最快的
+O(n^2)=
+"""
+
+# a = 5         1
+# b = 6         1
+# c = 10        1
+
+# for i in range(n):
+#     for j in range(n):      3*n^2
+#         x = i * i
+#         y = j * j
+#         z = i * j
+#
+# for k in range(n):       2n
+#     w = a * k +100
+#     v = b * b
+#
+# d = 300      1
